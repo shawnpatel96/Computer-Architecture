682ad58da173ce3ba10785e1181664d41f976fa8
shawn <shawnpatel196@gmail.com>
2020-07-22 20:59:10 -0500

    beutified run() by using branchtable

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -7,22 +7,61 @@ class CPU:
 
     def __init__(self):
         """Construct a new CPU."""
+        self.running = True
         self.ram = [0] * 256
         self.registers = [0] * 8
+        self.registers[7] = 0xF4
         self.pc = 0 # program counter,address of the currently executing instruction
         self.fl = 0 # holds the current flags status
         self.ir=  0
         #Commands
-        self.HLT = 0b00000001 # aka HALT, exit the emulator
-        self.LDI = 0b10000010 # aka LOAD, sets a value to a specified register.
-        self.PRN = 0b01000111 # aka PRINT, prints value of a register
-        self.MUL = 0b10100010 # aka MULTIPLY
+        self.branchtable = {}
+        self.branchtable[0b00000001] = self.HLT # aka HALT, exit the emulator
+        self.branchtable[0b10000010] = self.LDI # aka LOAD, sets a value to a specified register.
+        self.branchtable[0b01000111] = self.PRN # aka PRINT, prints value of a register
+        self.branchtable[0b10100010] = self.MUL # aka MULTIPLY
+        self.branchtable[0b01000101] = self.PUSH 
+        self.branchtable[0b01000110] = self.POP
+
+    def HLT(self):
+        self.running = False
+    def LDI(self):
+        reg_index = self.ram[self.pc + 1]
+        value_to_save = self.ram[self.pc + 2]
+        self.registers[reg_index] = value_to_save
+        self.pc += 1 + (self.ir >> 6)
+    def PRN(self):
+        reg_index = self.ram[self.pc + 1]
+        print(self.registers[reg_index])
+        self.pc += 1 + (self.ir >> 6)
+    def MUL(self):
+        operand_a = self.ram_read(self.pc + 1)
+        operand_b = self.ram_read(self.pc + 2)
+        self.alu('MUL', operand_a, operand_b)
+        self.pc += 1 + (self.ir >> 6)
+    def PUSH(self):
+        self.registers[7] -=1
+        sp = self.registers[7]  
+        reg_index = self.ram[self.pc+1]
+        value = self.registers[reg_index]
+        self.ram[sp] = value
+        self.pc +=1 +(self.ir >> 6)
+    def POP(self):
+        sp = self.registers[7]
+        value = self.ram[sp]
+        reg_index = self.ram[self.pc + 1]
+        self.registers[reg_index] = value
+        self.registers[7] += 1
+        self.pc += 1 + (self.ir >> 6)
+        
+
     # should accept the address to read and return the value stored there.
     def ram_read(self, index):
         return self.ram[index]
     # should accept a value to write, and the address to write it to.
     def ram_write(self, value, index):
         self.ram[index] = value
+
     def load(self):
         """Load a program into memory."""
 
@@ -42,6 +81,7 @@ class CPU:
                     if command == '':
                         continue
                     # after factoring append whatever is left to the program array
+                    # ideally that is the binary number
                     program.append(int(command, 2))
         except FileNotFoundError:
             print(f'{sys.argv[0]}: {sys.argv[1]} file was not found')
@@ -84,36 +124,14 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        running = True
-        while running:
-            # read memory at pc, set to instruction register
-            # this is the same as the memory[pc] array [PRINT_TIM, PRINT_TIM, HALT]
-            ir = self.ram_read(self.pc)
+        
+        while self.running:
 
-            # look at the following 1 or 2 bytes of memory, stored as variables..why exactly??
-            operand_a = self.ram_read(self.pc + 1)
-            operand_b = self.ram_read(self.pc + 2)
-
-            # if command = LDI
-            if ir == self.LDI:
-                #grab the memory address 1 past the command, here the value is the spot in ram to save the value
-                #   = memory[pc + 1]
-                reg_index = self.ram[self.pc + 1]
-                # Grab value 2 pass the command, this value will get stored in the ram slot above
-                value_to_save = self.ram[self.pc + 2]
-                self.registers[reg_index] = value_to_save
-                # increment program counter enough to pass the command + the slot + the value
-                self.pc +=2
-            if ir == self.MUL:
-                self.registers[self.ram_read(self.pc + 1)] *= self.registers[self.ram_read(self.pc + 2)]
-                self.pc +=2
-            if ir == self.PRN:
-                reg_index = self.ram[self.pc+1]
-                print(self.registers[reg_index])
-                self.pc +=1
-            
-                
-            if ir == self.HLT:
-                running = False
-
-            self.pc +=1
+            # # read memory at pc, set to instruction register
+            # # this is the same as the memory[pc] array [PRINT_TIM, PRINT_TIM, HALT]
+            self.ir = self.ram_read(self.pc)
+            try:
+                self.branchtable[self.ir]()
+            except KeyError:
+                print('Operation does not exist')
+                sys.exit()
--- scratch.py
+++ scratch.py
@@ -1,1 +1,121 @@
 
+PRINT_TIM    =  0b00000001
+HALT         =  0b10  # 2
+PRINT_NUM    =  0b00000011  # opcode 3
+SAVE         =  0b100
+PRINT_REG    =  0b101    # opcode 5
+ADD          =  0b110
+PUSH         =  0b111
+POP          =  0b1000   # opcode 8
+
+import sys
+
+memory = [0] * 256 
+
+def load_memory(file_name):
+    try:
+        address = 0
+        with open(file_name) as file:
+            for line in file:
+                split_line = line.split('#')[0]
+                command = split_line.strip()
+
+                if command == '':
+                    continue
+
+                instruction = int(command, 2)
+                memory[address] = instruction
+
+                address += 1
+
+    except FileNotFoundError:
+        print(f'{sys.argv[0]}: {sys.argv[1]} file was not found')
+        sys.exit()
+
+if len(sys.argv) < 2:
+    print("Please pass in a second filename: python3 in_and_out.py second_filename.py")
+    sys.exit()
+
+file_name = sys.argv[1]
+load_memory(file_name)
+
+# write a program to pull each command out of memory and execute
+# We can loop over it!
+
+# register aka memory
+registers = [0] * 8
+
+registers[7] = 0xF4
+
+# [0,0,99,0,0,0,0,0]
+# R0-R7
+
+
+pc = 0  # program counter
+running = True
+while running:
+    command = memory[pc]
+
+    if command == PRINT_TIM:
+        print("Tim!")
+
+    if command == HALT:
+        running = False
+
+    if command == PRINT_NUM:
+        num_to_print = memory[pc + 1]
+        print(num_to_print)
+        pc += 1
+
+    if command == SAVE:
+        reg = memory[pc + 1]
+        num_to_save = memory[pc + 2]
+        registers[reg] = num_to_save
+
+        pc += 2
+
+    if command == PRINT_REG:
+        reg_index = memory[pc + 1]
+        print(registers[reg_index])
+        pc += 1
+
+    if command == ADD:
+        first_reg = memory[pc + 1]
+        sec_reg = memory[pc + 2]
+        registers[first_reg] = registers[first_reg] + registers[sec_reg]
+        pc += 2
+
+    if command == PUSH:
+        # decrement the stack pointer
+        registers[7] -= 1
+
+        # get the register number
+        reg = memory[pc + 1]
+        # get a value from the given register
+        value = registers[reg]
+
+        # put the value at the stack pointer address
+        sp = registers[7]
+        memory[sp] = value
+
+        pc += 1
+
+        
+    if command == POP:
+       # get the stack pointer (where do we look?)
+       sp = registers[7]
+
+       # get register number to put value in
+       reg = memory[pc + 1]
+
+       # use stack pointer to get the value
+       value = memory[sp]
+       # put the value into the given register
+       registers[reg] = value
+        # increment our stack pointer
+       registers[7] += 1
+
+        # increment our program counter
+       pc += 1
+        
+    pc += 1
